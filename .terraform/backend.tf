50e18e5373f43d7d8bfa40a3f101c88e52c63ead
Henrique <henryphcog@gmail.com>
2025-04-18 17:12:08 -0300

    try new structure

--- .terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucket-henrique1"
-    key            = "nasaws/.terraform/terraform.tfstate"
-    region         = "sa-east-1"
-    dynamodb_table = "terraform-lock-table-henrique1"
-  }
-}
