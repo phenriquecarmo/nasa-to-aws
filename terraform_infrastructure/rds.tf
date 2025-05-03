71449c5fa8059dd78824397a52528fea72ff2e34
Henrique <henryphcog@gmail.com>
2025-05-03 17:04:57 -0300

    fix using entire name

--- terraform_infrastructure/rds.tf
+++ terraform_infrastructure/rds.tf
@@ -29,7 +29,7 @@ resource "aws_db_instance" "postgres" {
   allocated_storage       = 10
   storage_type            = "gp2"
   engine                  = "postgres"
-  engine_version          = "15.5"
+  engine_version = "15.5-R2"
   instance_class          = "db.t3.micro"
   db_name                 = "nasaws_db"
   username                = var.db_username
